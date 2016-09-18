88f65a7564d059382b99d405d46dbe5beb29a3bf
Shiny <brenda@wallace.net.nz>
2016-09-19 09:58:13 +1200

    Update index.js
    removed discus

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,17 +10,3 @@
         dataType: 'html',
         maxPage: 3,
     });
-/*
-===============================================================================
-  Disqus 
-===============================================================================
-*/
-(function () { // DON'T EDIT BELOW THIS LINE
-    var d = document,
-        s = d.createElement('script');
-
-    s.src = '//tilligettherecom.disqus.com/embed.js';
-
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-})();
